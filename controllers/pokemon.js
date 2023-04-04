2d2c4c5bac07d89966e698a2e07eb4a5b6e1597c
2manycooks <73556731+2manycooks@users.noreply.github.com>
2023-04-03 17:02:05 -0700

    get and post for faves working

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  try{
+    const foundPokeFaves = await db.pokemon.findAll()
+    res.render('pokemon/index', {
+      pokemon: foundPokeFaves
+    })
+  } catch(err) {
+    console.log(err)
+  }
+  
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  try{
+    const newPokemon = await db.pokemon.create({
+      name: req.body.name
+    })
+    console.log(`made new pokemon ${newPokemon}`)
+    res.redirect('pokemon')
+  } catch(err) {
+    console.log(err)
+  }
+  // res.send(req.body);
 });
 
 module.exports = router;
